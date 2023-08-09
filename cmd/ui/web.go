3b25778fbf47b6f4ecef8c8ae1f539c057ec3504
Jeremy <jeremy.rodarellis@gmail.com>
2023-08-10 08:53:34 +1000

    new version go backend only

--- cmd/ui/web.go
+++ cmd/ui/web.go
@@ -1,40 +1,20 @@
+// main.go
 package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/maxence-charriere/go-app/v9/pkg/app"
 )
 
-type myComponent struct {
-	app.Compo
-}
-
-func (c *myComponent) Render() app.UI {
-	return app.Div().Text("Hello, Go-App!")
-}
-
-func serveWebAssembly(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/wasm")
-	http.ServeFile(w, r, "web/app.wasm")
-}
-
 func main() {
-	app.Route("/", &myComponent{})
-	app.RunWhenOnBrowser()
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 
-	http.HandleFunc("/wasm_exec.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "wasm_exec.js")
-	})
+	http.HandleFunc("/api/save", saveFormData)
+	http.ListenAndServe(":8080", nil)
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
-
-	http.HandleFunc("web/app.wasm", serveWebAssembly)
-
-	fmt.Println("Server started at http://localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+func saveFormData(w http.ResponseWriter, r *http.Request) {
+	// Handle form data submission and store it
+	fmt.Println("Form data received on the server")
 }
