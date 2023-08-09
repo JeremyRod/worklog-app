bd9f770af1d6eff97774014f0598e735a50dc93d
Jeremy <jeremy.rodarellis@gmail.com>
2023-08-09 21:51:49 +1000

    Work completed saved ins session storage

--- cmd/ui/web.go
+++ cmd/ui/web.go
@@ -1,76 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
 )
 
-// hello is a component that displays a simple "Hello World!". A component is a
-// customizable, independent, and reusable UI element. It is created by
-// embedding app.Compo into a struct.
-type hello struct {
+type myComponent struct {
 	app.Compo
 }
 
-type newEntry struct {
-	app.Compo
-	button bool
+func (c *myComponent) Render() app.UI {
+	return app.Div().Text("Hello, Go-App!")
 }
 
-// The Render method is where the component appearance is defined. Here, a
-// "Hello World!" is displayed as a heading.
-func (h *hello) Render() app.UI {
-	return app.H1().Text("Hello World!")
+func serveWebAssembly(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/wasm")
+	http.ServeFile(w, r, "web/app.wasm")
 }
 
-func (n *newEntry) Render() app.UI {
-	return app.H1().Text("Hello World!")
-}
-
-// The main function is the entry point where the app is configured and started.
-// It is executed in 2 different environments: A client (the web browser) and a
-// server.
 func main() {
-	// The first thing to do is to associate the hello component with a path.
-	//
-	// This is done by calling the Route() function,  which tells go-app what
-	// component to display for a given path, on both client and server-side.
-	app.Route("/", &hello{})
-	app.Route("/newentry", &newEntry{})
-
-	// Once the routes set up, the next thing to do is to either launch the app
-	// or the server that serves the app.
-	//
-	// When executed on the client-side, the RunWhenOnBrowser() function
-	// launches the app,  starting a loop that listens for app events and
-	// executes client instructions. Since it is a blocking call, the code below
-	// it will never be executed.
-	//
-	// When executed on the server-side, RunWhenOnBrowser() does nothing, which
-	// lets room for server implementation without the need for precompiling
-	// instructions.
+	app.Route("/", &myComponent{})
 	app.RunWhenOnBrowser()
 
-	// Finally, launching the server that serves the app is done by using the Go
-	// standard HTTP package.
-	//
-	// The Handler is an HTTP handler that serves the client and all its
-	// required resources to make it work into a web browser. Here it is
-	// configured to handle requests with a path that starts with "/".
-	http.Handle("/", &app.Handler{
-		Name:        "Hello",
-		Description: "An Hello World! example",
+	http.HandleFunc("/wasm_exec.js", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "wasm_exec.js")
 	})
 
-	http.Handle("/newentry", &app.Handler{
-		Name:        "Hello!!!",
-		Description: "An Hello World! example",
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
 	})
 
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Println("Server Started")
-		log.Fatal(err)
-	}
+	http.HandleFunc("web/app.wasm", serveWebAssembly)
+
+	fmt.Println("Server started at http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
