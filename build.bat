f965c2da411fd31d20df3424933f074ccaa1ba74
JeremyRod <jeremy.rodarellis@gmail.com>
2024-10-14 12:38:51 +1100

    udpate: version

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=1.0.1
+set VERSION=1.0.2
 for /f %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT=%%i
 
 go build -ldflags "-X main.version=%VERSION% -X main.gitCommit=%GIT_COMMIT%" -o worklog.exe
