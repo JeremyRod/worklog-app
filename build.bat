42938fd3b60253f23f0e8fe3767917ab81389a13
jeremy rodarellis <jeremy.rodarellis@gmail.com>
2024-10-19 23:58:57 +1100

    update: verison
    change: split runner files

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=1.1.0
+set VERSION=1.1.2
 for /f %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT=%%i
 
 go build -ldflags "-X main.version=%VERSION% -X main.gitCommit=%GIT_COMMIT%" -o worklog.exe
