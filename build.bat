45571cfb018175eb95d07888d52c07c912aa7955
jeremy rodarellis <jeremy.rodarellis@gmail.com>
2024-10-20 14:43:51 +1100

    change: add linux and mac into the action
    update: build.bat to provide more detail on windows binary

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 set VERSION=1.1.2
 for /f %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT=%%i
 
-go build -ldflags "-X main.version=%VERSION% -X main.gitCommit=%GIT_COMMIT%" -o worklog-%VERSION%.exe
+go build -ldflags "-X main.version=%VERSION% -X main.gitCommit=%GIT_COMMIT%" -o worklog-%1-%2-%VERSION%.exe
