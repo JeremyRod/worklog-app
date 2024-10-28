7bb67a4e8a02baaa296df9b8b2e1e9b61038a7be
jeremy rodarellis <jeremy.rodarellis@gmail.com>
2024-10-28 22:31:21 +1100

    update: build version update

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=1.1.2
+set VERSION=1.1.4
 for /f %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT=%%i
 
 go build -ldflags "-X main.version=%VERSION% -X main.gitCommit=%GIT_COMMIT%" -o worklog-%1-%2-%VERSION%.exe
